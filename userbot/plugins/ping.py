f0c6c5960671d3efc53f6a97bbb58661220e3f40
powerbot1776 <83866195+powerbot1776@users.noreply.github.com>
2021-05-09 12:07:46 +0530

    Update ping.py

--- userbot/plugins/ping.py
+++ userbot/plugins/ping.py
@@ -8,7 +8,7 @@ async def _(event):
     if event.fwd_from:
         return
     start = datetime.now()
-    event = await edit_or_reply(event, "Pong!")
+    event = await edit_or_reply(event, "LAUDA!")
     end = datetime.now()
     ms = (end - start).microseconds / 1000
     await event.edit("Pong!\n`{} ms`".format(ms))
